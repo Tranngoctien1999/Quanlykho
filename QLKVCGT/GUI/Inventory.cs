caf70733fbcb6fe19502f57b70b16d860c94cbf6
phamxuantien27 <65357044+phamxuantien27@users.noreply.github.com>
2020-11-29 04:30:27 -0800

    xuan tien add inventory nhe

---
+++ QLKVCGT/GUI/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using QLKVCGT.Controler;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLKVCGT.GUI
+{
+    public partial class Inventory : DevExpress.XtraEditors.XtraForm
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+        
+        private void loadThongKeNhoNho()
+        {
+            try
+            {
+                tbMin.Text = InventoryControl.Instance.DataSource_getMaxStored().Rows[0]["TENHH"].ToString();
+                tbMax.Text = InventoryControl.Instance.DataSource_getMinStored().Rows[0]["TENHH"].ToString();
+                tbTotalStored.Text = InventoryControl.Instance.DataSource_getTotalPriceStored().Rows[0]["Total"].ToString();
+                cbbListItemForBuy.DataSource = InventoryControl.Instance.DataSource_getItemForBuy().Rows[0]["TENHH"].ToString();
+                cbbListItemForBuy.DisplayMember = "TENHH";
+            }
+            catch { }
+        }
+
+        private void Inventory_Load(object sender, EventArgs e)
+        {
+            loadThongKeNhoNho();
+        }
+    }
+}
