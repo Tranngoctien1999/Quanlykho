73d4c5c421a157069d4deadb96184b897c10fd86
ngoctien <ngoctien18051999@gmail.com>
2020-11-29 19:43:46 +0700

    add controller

---
+++ QLKVCGT/Controler/IssueVouControl.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKVCGT.Controler
+{
+    class IssueVouControl
+    {
+        private static IssueVouControl instance;
+
+        public static IssueVouControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new IssueVouControl();
+                return instance;
+            }
+
+            private set
+            {
+                instance = value;
+            }
+        }
+        private IssueVouControl() { }
+
+       
+        public DataTable DataSource_GetListBill()
+        {
+            string query = @"exec SP_getListBill ";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { });
+        }
+        public int InsertBill(int _employessId, string _customerId, int _totalPrice)
+        {
+            string query = @"exec SP_insertBill @employeesId, @customerId, @totalPrice";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _employessId, _customerId, _totalPrice });
+        }
+
+
+        public int InsertBillDetail(int _itemId, int _billId, int _numOfItem)
+        {
+            string query = @"exec SP_insertBillDetail @itemId, @billId, @numOfItem";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _itemId, _billId, _numOfItem});
+        }
+
+        public DataTable DataSoure_GetMaxBillByEmpId(int _employeesId)//lay ra cai hoa don moi nhat cau nhan vien
+        {
+            string query = @"exec SP_getMaxBillForEmp @employeesId";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { _employeesId });
+
+        }
+
+        public DataTable DataSource_GetDetailBillByID(int id)
+        {
+            DataTable dt = new DataTable();
+            string query = @"exec SP_getBillDetail @id";
+            dt = DataProvider.Instances.ExecuteQuery(query, new object[] { id });
+            return dt;
+        }
+
+        public void UpdateBill(int _billId, int _totalPrice)
+        {
+            string query = @"exec SP_updateBill @billId, @totalPrice";
+            DataProvider.Instances.ExecuteQuery(query, new object[] { _billId, _totalPrice });
+        }
+
+        public int DeleteBill(int _billId)
+        {
+            string query = @"exec SP_deleteBill @billId";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _billId });
+        }
+    }
+}
---
+++ QLKVCGT/Controler/ItemControl.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLKVCGT.Controler
+{
+    class ItemControl
+    {
+        private static ItemControl instance;
+
+        public static ItemControl Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ItemControl();
+                return instance;
+            }
+            set
+            {
+                instance = value;
+            }
+        }
+        private ItemControl() { }
+       
+        public DataTable DataSource_GetItem()
+        {
+            string query = @"exec  SP_getItem ";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] {  });
+        }
+        public DataTable DataSource_GetItemByIdItem(int id)
+        {
+            string query = @"exec  SP_getItemByIdItem @id";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { id });
+        }
+        public DataTable DataSource_GetItemByName(string name)
+        {
+            string query = @"exec  SP_getItemByName @name";
+            return DataProvider.Instances.ExecuteQuery(query, new object[] { name });
+        }
+        public int InsertItem(string _itemName, int _numberOfStored, string _once, int _price)
+        {
+            string query = @"exec SP_insertItem @itemName, @numberOfStored, @once, @price";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _itemName, _numberOfStored, _once, _price });
+        }
+        public int UpdateItem(string _itemName, int _numberOfStored)
+        {
+            string query = @"exec SP_updateItem @itemName, @numberOfStored";
+            return DataProvider.Instances.ExecuteNonQuery(query, new object[] { _itemName, _numberOfStored });
+        }
+
+    }
+}
