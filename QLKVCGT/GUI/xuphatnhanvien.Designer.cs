a0e69c9367707fcba72eb8e4d5ec9f6618049287
NguyenCongVN <47655418+NguyenCongVN@users.noreply.github.com>
2020-12-04 10:03:25 +0700

    Revert "Revert "Them form xuphatnhanvien""
    This reverts commit e3f78623c79b8e2c1233c25ce32ae0067e3b849c.

---
+++ QLKVCGT/GUI/xuphatnhanvien.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace QLKVCGT.GUI
+{
+    partial class xuphatnhanvien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(xuphatnhanvien));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEdit = new DevExpress.XtraEditors.SimpleButton();
+            this.btnDelete = new DevExpress.XtraEditors.SimpleButton();
+            this.btnCancel = new DevExpress.XtraEditors.SimpleButton();
+            this.btnAdd = new DevExpress.XtraEditors.SimpleButton();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.gctListEmployees = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbma = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbten = new System.Windows.Forms.TextBox();
+            this.label16 = new System.Windows.Forms.Label();
+            this.tbmucphat = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gctListEmployees)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(13, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1153, 499);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnSave);
+            this.panel2.Controls.Add(this.btnEdit);
+            this.panel2.Controls.Add(this.btnDelete);
+            this.panel2.Controls.Add(this.btnCancel);
+            this.panel2.Controls.Add(this.btnAdd);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Location = new System.Drawing.Point(-100, -46);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1353, 591);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(233, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(193, 21);
+            this.label1.TabIndex = 82;
+            this.label1.Text = "XỬ PHẠT NHÂN VIÊN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Appearance.Options.UseFont = true;
+            this.btnSave.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(371, 457);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 39);
+            this.btnSave.TabIndex = 81;
+            this.btnSave.Text = "Lưu";
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Appearance.Options.UseFont = true;
+            this.btnEdit.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.btnEdit.Image = ((System.Drawing.Image)(resources.GetObject("btnEdit.Image")));
+            this.btnEdit.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnEdit.Location = new System.Drawing.Point(293, 383);
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(94, 39);
+            this.btnEdit.TabIndex = 80;
+            this.btnEdit.Text = "Sửa";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Appearance.Options.UseFont = true;
+            this.btnDelete.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.btnDelete.Image = ((System.Drawing.Image)(resources.GetObject("btnDelete.Image")));
+            this.btnDelete.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnDelete.Location = new System.Drawing.Point(447, 383);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(94, 39);
+            this.btnDelete.TabIndex = 79;
+            this.btnDelete.Text = "Xóa";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Appearance.Options.UseFont = true;
+            this.btnCancel.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.btnCancel.Image = ((System.Drawing.Image)(resources.GetObject("btnCancel.Image")));
+            this.btnCancel.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnCancel.Location = new System.Drawing.Point(208, 457);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(94, 39);
+            this.btnCancel.TabIndex = 78;
+            this.btnCancel.Text = "Hủy";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Appearance.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Appearance.Options.UseFont = true;
+            this.btnAdd.ButtonStyle = DevExpress.XtraEditors.Controls.BorderStyles.Style3D;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageLocation = DevExpress.XtraEditors.ImageLocation.MiddleLeft;
+            this.btnAdd.Location = new System.Drawing.Point(132, 383);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 39);
+            this.btnAdd.TabIndex = 77;
+            this.btnAdd.Text = "Thêm";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.gctListEmployees);
+            this.panel3.Location = new System.Drawing.Point(620, 69);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(633, 480);
+            this.panel3.TabIndex = 3;
+            // 
+            // gctListEmployees
+            // 
+            this.gctListEmployees.Dock = System.Windows.Forms.DockStyle.Fill;
+            // 
+            // 
+            // 
+            this.gctListEmployees.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gctListEmployees.Location = new System.Drawing.Point(0, 0);
+            this.gctListEmployees.MainView = this.gridView1;
+            this.gctListEmployees.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gctListEmployees.Name = "gctListEmployees";
+            this.gctListEmployees.Size = new System.Drawing.Size(633, 480);
+            this.gctListEmployees.TabIndex = 0;
+            this.gctListEmployees.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gctListEmployees;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsDetail.DetailMode = DevExpress.XtraGrid.Views.Grid.DetailMode.Default;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label4);
+            this.panel7.Controls.Add(this.tbma);
+            this.panel7.Controls.Add(this.label9);
+            this.panel7.Controls.Add(this.tbten);
+            this.panel7.Controls.Add(this.label16);
+            this.panel7.Controls.Add(this.tbmucphat);
+            this.panel7.Location = new System.Drawing.Point(143, 125);
+            this.panel7.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(389, 136);
+            this.panel7.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(15, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 21);
+            this.label4.TabIndex = 66;
+            this.label4.Text = "Mã xử phạt:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbma
+            // 
+            this.tbma.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbma.Location = new System.Drawing.Point(138, 8);
+            this.tbma.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbma.Name = "tbma";
+            this.tbma.Size = new System.Drawing.Size(203, 23);
+            this.tbma.TabIndex = 65;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label9.Location = new System.Drawing.Point(15, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(118, 21);
+            this.label9.TabIndex = 54;
+            this.label9.Text = "Tên xử phạt:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbten
+            // 
+            this.tbten.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbten.Location = new System.Drawing.Point(138, 38);
+            this.tbten.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbten.Name = "tbten";
+            this.tbten.Size = new System.Drawing.Size(203, 23);
+            this.tbten.TabIndex = 53;
+            // 
+            // label16
+            // 
+            this.label16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label16.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label16.Location = new System.Drawing.Point(15, 70);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(103, 21);
+            this.label16.TabIndex = 56;
+            this.label16.Text = "Mức phạt";
+            this.label16.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // tbmucphat
+            // 
+            this.tbmucphat.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbmucphat.Location = new System.Drawing.Point(138, 68);
+            this.tbmucphat.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.tbmucphat.Name = "tbmucphat";
+            this.tbmucphat.Size = new System.Drawing.Size(203, 23);
+            this.tbmucphat.TabIndex = 55;
+            // 
+            // xuphatnhanvien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1259, 514);
+            this.Controls.Add(this.panel1);
+            this.Name = "xuphatnhanvien";
+            this.Text = "viphamkhachhang";
+            this.Load += new System.EventHandler(this.xuphatnhanvien_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gctListEmployees)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+        private DevExpress.XtraEditors.SimpleButton btnEdit;
+        private DevExpress.XtraEditors.SimpleButton btnDelete;
+        private DevExpress.XtraEditors.SimpleButton btnCancel;
+        private DevExpress.XtraEditors.SimpleButton btnAdd;
+        private System.Windows.Forms.Panel panel3;
+        private DevExpress.XtraGrid.GridControl gctListEmployees;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbma;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbten;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox tbmucphat;
+    }
+}
---
+++ QLKVCGT/GUI/xuphatnhanvien.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using QLKVCGT.Controler;
+
+namespace QLKVCGT.GUI
+{
+    public partial class xuphatnhanvien : Form
+    {
+        private int action;
+        private int type;
+        public xuphatnhanvien()
+        {
+            InitializeComponent();
+        }
+        private void ReadOnly(bool e)
+        {
+           
+            tbma.ReadOnly = e;
+            tbten.Enabled = !e;
+            tbmucphat.Enabled = !e;
+           
+
+        }
+        private void EnabledBtn(bool e)
+        {
+            btnAdd.Enabled = e;
+            btnEdit.Enabled = e;
+            btnDelete.Enabled = e;
+            btnCancel.Enabled = !e;
+            btnSave.Enabled = !e;
+        }
+        private void clear()
+        {
+            
+            tbma.Clear();
+            tbten.Clear();
+            tbmucphat.Clear();
+           
+        }
+        private void loadEmployeesToDgv()
+        {
+            gctListEmployees.DataSource = xuphatnhanviencs.Instance.xuphatnhanvien();
+        }
+         private void binding()
+        {
+            tbma.DataBindings.Clear();
+            tbma.DataBindings.Add("Text", gctListEmployees.DataSource, "Mã xử phạt");
+            tbten.DataBindings.Clear();
+            tbten.DataBindings.Add("Text", gctListEmployees.DataSource, "Tên xử phạt");
+            tbmucphat.DataBindings.Clear();
+            tbmucphat.DataBindings.Add("Text", gctListEmployees.DataSource, "Mức phạt");
+            
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void xuphatnhanvien_Load(object sender, EventArgs e)
+        {
+            EnabledBtn(true);
+            ReadOnly(true);
+            loadEmployeesToDgv();
+            binding();
+            //pnlHide.Hide();
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            action = 1;
+            EnabledBtn(false);
+            ReadOnly(false);
+            tbten.ReadOnly = true;
+            clear();
+        }
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            action = 2;
+            EnabledBtn(false);
+            ReadOnly(true);
+            tbten.ReadOnly = false;
+            //pnlHide.Show();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            action = 3;
+            ReadOnly(true);
+            tbten.ReadOnly = false;
+            EnabledBtn(false);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            xuphatnhanvien_Load(sender, e);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string _ma = tbma.Text.Trim(' ');
+            string _ten = tbten.Text.ToString().Trim(' ');
+            string _mucphat = tbmucphat.Text.ToString().Trim(' ');
+            
+            if (action == 1)
+            {
+                if (_ten == "" || _ma == "" || _mucphat == "")
+                {
+                    MessageBox.Show("Vui lòng điền đầy đủ thông tin !");
+                    return;
+                }
+                try
+                {
+                    xuphatnhanviencs.Instance.insertmucphat(_ma, _ten, _mucphat);
+                }
+                catch { MessageBox.Show("Trùng tên"); }
+
+
+            }
+            if (action == 2)
+            {
+                xuphatnhanviencs.Instance.updatemucphat(_ma,_ten, _mucphat);
+
+            }
+            if (action == 3)
+            {
+                xuphatnhanviencs.Instance.deletemucphat(_ma);
+            }
+            xuphatnhanvien_Load(sender, e);
+        }
+
+       
+
+    }
+}
